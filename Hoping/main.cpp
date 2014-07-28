54f4eb1fd96ee6e2f3c984693e520751d773a812
wangyu <wangyu@wangyudembp.lan>
2014-07-28 19:01:46 +0800

    Initial Commit

---
+++ Hoping/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hoping
+//
+//  Created by wangyu on 14-7-28.
+//  Copyright (c) 2014年 tz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
